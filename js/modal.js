dbbe3d40f707ec35aecc269a53ab935bb5eee3ee
Anna-Buzuk <78303888+Anna-Buzuk@users.noreply.github.com>
2022-06-08 18:11:07 +0200

    Kliknięcie poza popupa, automatycznie go zamyka

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,19 @@
 var modal = document.getElementById("mymodal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close") [0];
+var btn = document.getElementById("btn_modal_window");
+var close = document.getElementsByClassName("close_modal_window") [0];
+
+
 
 btn.onclick = function () {
     modal.style.display = "block";
 }
-span.onclick = function() {
+close.onclick = function() {
     modal.style.display = "none";
 }
 
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
