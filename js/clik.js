a22b09c6a270a7385bf22854c4ec124ab7b603d1
Anna-Buzuk <78303888+Anna-Buzuk@users.noreply.github.com>
2022-06-08 18:25:28 +0200

    Każde kliknięcie przycisku musi być zliczane

---
+++ js/clik.js
@@ -0,0 +1,11 @@
+ const btn_modal_window = document.querySelector('.button')
+ function hendleBtnClick(event) {
+    event.preventDefault();
+    let counteClickBtn = btn_modal_window.textContent;
+    btn_modal_window.texContent = parseInt(counteClickBtn) + 1;
+
+ }
+
+ btn_modal_window,addEventListener('click', hendleBtnClick );
+
+ console.log(btn_modal_window);
